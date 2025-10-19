3b7bb8df606c34908cc39cf0300cf6fd6e5e5848
Tanay Vasishtha <tanayvasishtha@gmail.com>
2025-10-19 23:49:33 +0530

    fix: allow Spotify and Google iframes in CSP

--- next.config.mjs
+++ next.config.mjs
@@ -36,7 +36,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' https://open.spotify.com https://www.google.com; object-src 'none'; base-uri 'self'; form-action 'self';"
           }
         ]
       }
